1f20298db1a526c7b41247d2d2bb60fcaaa234ef
MD Shoaib Khan <abrohoman019@gmail.com>
2025-05-19 22:08:40 +0600

    Add EvenNumberMultiThread class for concurrent even number generation

---
+++ Que_Solve/EvenNumberMultiThread.java
@@ -0,0 +1,61 @@
+package Que_Solve;
+
+
+class ThreadEven extends Thread {
+    int start;
+    int end;
+    int[] Arr;
+    int index;
+
+    ThreadEven(int start, int end, int[] Arr, int index) {
+        this.start = start;
+        this.end = end;
+        this.Arr = Arr;
+        this.index = index;
+    }
+
+    public void run() {
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                Arr[index] = i;
+                index++;
+                // try {
+                //     Thread.sleep(10);
+                // } catch (InterruptedException e) {
+                //     System.out.println("Thread interrupted");
+                // }
+            }
+        }
+    }
+}
+
+public class EvenNumberMultiThread {
+    public static void main(String[] args) {
+        int[] evenNumbers = new int[24]; 
+
+        ThreadEven t1 = new ThreadEven(20, 30, evenNumbers, 0);
+        ThreadEven t2 = new ThreadEven(40, 50, evenNumbers, 6);
+        ThreadEven t3 = new ThreadEven(60, 70, evenNumbers, 12);
+        ThreadEven t4 = new ThreadEven(80, 90, evenNumbers, 18);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted");
+        }
+
+        // Print the final array
+        System.out.println("Even numbers added by threads:");
+        for (int i = 0; i < evenNumbers.length; i++) {
+            System.out.print(evenNumbers[i] + " ");
+        }
+    }
+}
